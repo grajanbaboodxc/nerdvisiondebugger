33081d68db819ee0c7cb10126e3fce6a1903b575
grajanbaboodxc <53206575+grajanbaboodxc@users.noreply.github.com>
2019-07-23 10:31:44 +0530

    Create testdebug1.py

---
+++ testdebug1.py
@@ -0,0 +1,16 @@
+import nerdvision
+
+nerdvision.start('894fda5c8890307ffcf846ae8c903a624a5027e89280e5d83b7feea0cba2f7fcd7019b1175e6b8e6a114b91969d0c0027295909eed01807e5ff4ddbb69598654')
+
+
+def stringvertprint(inpstr):
+    for i in inpstr:
+        print(i)
+
+def main():
+    stringvertprint("Hello World")
+
+if __name__ == "__main__":
+    main()
+    
+    
